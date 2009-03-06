53a281f73976a88ad26e8d55930936a89f380561
anderson.birocchi <anderson.birocchi@f1493576-09c1-11de-a1f0-819f45317607>
2009-03-06 15:12:13 +0000

    Função que cria, apaga e imprime a matriz arrumadas e funcionais

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ all:
 	gcc -Wall -g -o jogovida funcoes.c main.c jogovida.c
 
 clean:
-	rm criamatriz
+	rm jogovida
--- funcoes.c
+++ funcoes.c
@@ -1,54 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <malloc.h>
 
 /* Aloca memoria para uma matriz de tamanho recebido como parametro.
  * -Parametros: Numero de linhas, Numero de colunas, Endereço da variavel
  * que armazenara a matriz.
  */
-int Cria_Matriz(int num_lin, int num_col, int **Matriz){
+int Cria_Matriz(int num_lin, int num_col, int ***Matriz){
 
-  *Matriz = malloc(sizeof(int)*num_lin*num_col);
+  int i;
+
+  *Matriz = (int**)malloc(sizeof(int)*num_lin);
+  for(i=0; i<num_lin; i++)
+    (*Matriz)[i]= (int*)malloc(sizeof(int)*num_col);
+
   if(Matriz == NULL){
     printf("Erro ao alocar memória");
     return 1;
   }
-  else
+  else{
     return 0;
+  }
 }
 
 
 /* Desaloca uma matriz criada na memoria.
- * -Parametros: Endereco da variavel que guarda a matriz
+ * -Parametros: Numero de linhas, Endereco da variavel que guarda a matriz
  */
-int Apaga_Matriz(int **Matriz){
+int Apaga_Matriz(int num_linhas, int ***Matriz){
+
+  int i;
+
+  for(i=0; i<num_linhas; i++)
+    free((*Matriz)[i]);
   free(*Matriz);
   return 0;
 }
 
 
+int Imprime_Matriz(int num_lin, int num_col, int **Matriz){
+  
+  int i,j;
+
+  for(i=0; i<num_lin; i++){
+    for(j=0; j<num_col; j++){
+      printf("|%d",Matriz[i][j]);
+    }
+    printf("|\n");
+  }
+}
+
+
 /*--Main para teste das funcoes--*/
-/*int main (){
+int main (){
 
   int i;
   int Lin = 4;
   int Col = 4;
-  int *Matriz;
+  int **Matriz;
 
   Cria_Matriz(Lin,Col,&Matriz);
   printf("Matriz %d x %d criada\n", Lin, Col);
+  Imprime_Matriz(Lin,Col,Matriz);
 
-  for(i=0; i<(Lin*Col); i++){
-    printf("i= %d\n", i);
-    Matriz[i] = i+100; 
-    printf("Matriz[i]= %d\n", Matriz[i]); 
-  }
-  for(i=0; i<(Lin*Col); i++)
-    printf("%d|", Matriz[i]);
+  Matriz[0][0] = 4;
+  Matriz[1][1] = 5;
+  Matriz[2][2] = 6;
+  Matriz[3][3] = 7;
+
   printf("\n");
+  Imprime_Matriz(Lin,Col,Matriz);
 
-  Desaloca_Matriz(&Matriz);
+  Apaga_Matriz(Lin,&Matriz);
   return 0;
-  }*/
-
-
+}
