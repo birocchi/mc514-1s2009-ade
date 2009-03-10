2ebe2042012940915ca232ea005464cd8d2c9aad
anderson.birocchi <anderson.birocchi@f1493576-09c1-11de-a1f0-819f45317607>
2009-03-10 21:20:52 +0000

    Arrumada as funcoes d manipulaçao da matriz

--- funcoes.c
+++ funcoes.c
@@ -6,13 +6,13 @@
  * -Parametros: Numero de linhas, Numero de colunas, Endereço da variavel
  * que armazenara a matriz.
  */
-int Cria_Matriz(int num_lin, int num_col, int ***Matriz){
+int Cria_Matriz(int num_lin, int num_col, char ***Matriz){
 
   int i;
 
-  *Matriz = (int**)malloc(sizeof(int)*num_lin);
+  *Matriz = (char**)malloc(sizeof(char*)*num_lin);
   for(i=0; i<num_lin; i++)
-    (*Matriz)[i]= (int*)malloc(sizeof(int)*num_col);
+    (*Matriz)[i]= (char*)malloc(sizeof(char)*num_col);
 
   if(Matriz == NULL){
     printf("Erro ao alocar memória");
@@ -27,7 +27,7 @@ int Cria_Matriz(int num_lin, int num_col, int ***Matriz){
 /* Desaloca uma matriz criada na memoria.
  * -Parametros: Numero de linhas, Endereco da variavel que guarda a matriz
  */
-int Apaga_Matriz(int num_linhas, int ***Matriz){
+int Apaga_Matriz(int num_linhas, char ***Matriz){
 
   int i;
 
@@ -38,35 +38,50 @@ int Apaga_Matriz(int num_linhas, int ***Matriz){
 }
 
 
-int Imprime_Matriz(int num_lin, int num_col, int **Matriz){
+int Imprime_Matriz(int num_lin, int num_col, char **Matriz){
   
   int i,j;
 
   for(i=0; i<num_lin; i++){
     for(j=0; j<num_col; j++){
-      printf("|%d",Matriz[i][j]);
+      printf("|%c",Matriz[i][j]);
     }
     printf("|\n");
   }
+  return 0;
 }
+
+int Inicializa_Matriz(int num_lin, int num_col, char **Matriz){
+  
+  int i,j;
+
+  for(i=0; i<num_lin; i++){
+    for(j=0; j<num_col; j++){
+      Matriz[i][j]=' ';
+    }
+  }
+  return 0;
+}
+
 
 
 /*--Main para teste das funcoes--*/
 int main (){
 
-  int i;
-  int Lin = 4;
-  int Col = 4;
-  int **Matriz;
+  int Lin = 5;
+  int Col = 5;
+  char **Matriz;
 
   Cria_Matriz(Lin,Col,&Matriz);
   printf("Matriz %d x %d criada\n", Lin, Col);
+  Inicializa_Matriz(Lin,Col,Matriz);
   Imprime_Matriz(Lin,Col,Matriz);
 
-  Matriz[0][0] = 4;
-  Matriz[1][1] = 5;
-  Matriz[2][2] = 6;
-  Matriz[3][3] = 7;
+  Matriz[0][0] = '#';
+  Matriz[1][1] = '#';
+  Matriz[2][2] = '#';
+  Matriz[3][3] = '#';
+  Matriz[4][4] = '#';
 
   printf("\n");
   Imprime_Matriz(Lin,Col,Matriz);
