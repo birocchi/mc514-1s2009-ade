a79e27715ecb20a91557c40547cee27f1945ea18
felipe.eltermann <felipe.eltermann@f1493576-09c1-11de-a1f0-819f45317607>
2009-03-12 00:56:20 +0000

    Adicionei algumas coisas...

--- main.c
+++ main.c
@@ -3,10 +3,13 @@
 
 
 #include "matriz.h"
+#include "threads.h"
+#include "interface.h"
+
 
 int main () {
 
-  int Linhas, Colunas, i;
+  int Linhas, Colunas, i, j;
   char **tab0, **tab1; /* matrizes que representam o tabuleiro */
   int n0; /* numero inicial de celulas vivas, obtido na interface */
   int n_thr; /* n de threads que serao executadas simultaneamente */
@@ -18,44 +21,37 @@ int main () {
 
 
   /* Alocacao da mamoria para as matrizes */
-  if( Aloca_Matriz(Linhas,Colunas,&tab0) || Aloca_Matriz(Linhas,Colunas,&tab1) ) {
-
-    /* TRATAMENTO DO ERRO AO ALOCAR MEMORIA PARA AS MATRIZES... */
-
+  if( Aloca_Matriz(Linhas,Colunas,&tab0) || Aloca_Matriz(Linhas,Colunas,&tab1) ){
+    /* Tratamento de erro na alocacao -> finaliza execucao */
+    printf("O programa sera finalizado.\n\n");
+    return(0);
   }
 
-  /* Imprime o tabuleiro inicial */
 
   /* Inicializa matriz tab0 com os valores de entrada, ou
    se for o caso, com os valores 'default' */
-  
-  
-  while(i!=0){
-    
-    /* INICIO DAS CHAMADAS DE THREADS */
 
-    /* Calculo de n_thr inicial
-       n_thr <- max{LINHAS, COLUNAS}
-       n_thr <- min{n_thr, PTHREAD_THREADS_MAX}  */
+
+  /* Imprime o tabuleiro inicial */
+  j=0;
+  Imprime_Matriz(Linhas,Colunas,tab0,j);
+   
+  n_thr=Calcula_Threads(Linhas,Colunas);
+
+  for(j=1;j<=i;j++){
     
-    if (Linhas>Colunas) n_thr = Linhas;
-    else n_thr = Colunas;
-    
-    if (n_thr>PTHREAD_THREADS_MAX) n_thr = PTHREAD_THREADS_MAX;
-    
-    
-    /* Chamadas das threads que processarao a matriz
+    /* Chamadas das threads que processarao a matriz:
        tab0 (estado atual) -> tab1 (proximo estado)  */
     
     
     /* imprime tab1*/
+    Imprime_Matriz(Linhas,Colunas,tab1,j);
     
     
     tmp=tab1;
     tab0=tab1; /* atualiza o estado para a proxima iteracao */
     tab1=tmp;
     
-    i--; /* i: numero de iteracoes restantes */
     
   }
   
