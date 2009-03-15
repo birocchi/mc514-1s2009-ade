c26600e2a5eb54f7457ee828c7ad4a7bab1ae55e
david.kurka <david.kurka@f1493576-09c1-11de-a1f0-819f45317607>
2009-03-15 22:59:48 +0000

    pronto! considero terminada a minha parte! mais detalhes no email abaixo...

--- threads.h
+++ threads.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int Calcula_Threads(int lin, int col);
 void calcula_prox(char* linhas[4],int tam);
 int conta_vizinho(int i, char* linhas[4]);
 void * thread(void * dados);
+void trata_bordas(int lin, int col, char **Matriz);
