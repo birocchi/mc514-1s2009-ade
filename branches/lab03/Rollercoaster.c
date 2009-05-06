de7fa7ab04bb6a56f2750a6ec626a87dfcad0ba3
anderson.birocchi <anderson.birocchi@f1493576-09c1-11de-a1f0-819f45317607>
2009-05-06 16:14:55 +0000

    Compilou o carro =)

--- branches/lab03/Rollercoaster.c
+++ branches/lab03/Rollercoaster.c
@@ -8,25 +8,6 @@
 #include <semaphore.h>
 #include <limits.h>
 
-/*#include <linux/futex.h>
-  #include <sys/syscall.h>      Não sei se vou usar futex...*/
-
-/*
-/* Retorna -1 se o futex não bloqueou e 
-            0 caso contrário *
-int futex_wait(void *addr, int val1) {
-  return syscall(SYS_futex, addr, FUTEX_WAIT, 
-                 val1, NULL, NULL, 0);
-}
-
-/* Retorna o número de threads que foram acordadas *
-int futex_wake(void *addr, int n) {
-    return syscall(SYS_futex, addr, FUTEX_WAKE, 
-                   n, NULL, NULL, 0);
-}
-*/
-
-
 /* ----Definicoes----*/
 #define N_CARROS 5
 #define N_PASSAGEIROS 50
@@ -44,7 +25,7 @@ sem_t plataforma_desembarque[N_CARROS];
 
 /*Semaforos que controlam a fila de (des)embarque dos passageiros*/
 sem_t fila_embarque;
-sem_t fila_desembarque
+sem_t fila_desembarque;
 
 /*Guardam quantos passageiros (des)embarcaram no carro*/
 volatile int embarcaram;
@@ -55,8 +36,8 @@ sem_t carro_encheu;
 sem_t carro_esvaziou;
 
 /*Mutexes que controlam o (des)embarque de um único passageiro por vez*/
-mutex_t trava_passageiros_embarque; 
-mutex_t trava_passageiros_desembarque;
+pthread_mutex_t trava_passageiros_embarque;
+pthread_mutex_t trava_passageiros_desembarque;
 
 /*----------------------------------------*/
 
@@ -64,51 +45,54 @@ mutex_t trava_passageiros_desembarque;
 
 /* ----Funcoes que serao utilizadas pelos carros---- */
 
-/*Funcao principal que controla os carros*/
-void* Carro(void *id) {
-  /*Por enquanto é isso que planejo para os carros, espero que tenha exclusão mutua e sem deadlock*/
-  /*
-  while(1){
-    sem_wait(plataforma_embarque[id]);
-    carrega();
-    sem_post(fila_de_embarque); /*Tem que dar post LIMITE_CARRO vezes*
-    sem_wait(carro_encheu);
-    sem_post(plataforma_embarque[proximo(id)]);
-    passeia();
-    sem_wait(plataforma_desembarque[id]);
-    descarrega();
-    sem_post(fila_desembarque); /*Tem que dar post LIMITE_CARRO vezes*
-    sem_wait(carro_esvaziou);
-    sem_post(plataforma_desembarque[proximo(id)]);
-  }
-  */
-  return NULL;
-}
-
 /*Calcula qual eh o proximo carro*/
 int proximo(int id) {
   return ( (id+1) % N_CARROS);
 }
 
 /*Carrega o carro com passageiros*/
-void* carrega(void *v) {
-  
+void* carrega() {
 
   return NULL;
 }
 
 /*Descarrega o carro*/
-void* descarrega(void *v) {
-  
+void* descarrega() {
 
   return NULL;
 }
 
 /*O carro passeia pelos trilhos*/
-void* passeia(void *v) {
+void* passeia() {
       sleep(rand() % 5);
   return NULL;
 }
+
+/*Funcao principal que controla os carros*/
+void* Carro(void *v) {
+  /*Por enquanto é isso que planejo para os carros, espero que tenha exclusão mutua e sem deadlock*/
+  
+  int k;
+  int id = (int) v;
+  while(1){
+    sem_wait(&plataforma_embarque[id]);
+    carrega();
+    for(k=0;k < LIMITE_CARRO; k++)
+      sem_post(&fila_embarque);
+    sem_wait(&carro_encheu);
+    sem_post(&plataforma_embarque[proximo(id)]);
+    passeia();
+    sem_wait(&plataforma_desembarque[id]);
+    descarrega();
+    for(k=0;k < LIMITE_CARRO; k++)
+      sem_post(&fila_desembarque);
+    sem_wait(&carro_esvaziou);
+    sem_post(&plataforma_desembarque[proximo(id)]);
+  }
+  
+  return NULL;
+}
+
 /*---------------------------------------------------*/
 
 
@@ -121,13 +105,13 @@ void* Passageiro(void *v) {
 /*
   sem_wait(fila_embarque);
   embarcar();
-  mutex_lock(trava_passageiros_embarque);
+  pthread_mutex_lock(trava_passageiros_embarque);
     embarcaram += 1;
     if (embarcaram == LIMITE_CARRO){
       sem_post(carro_encheu);
       embarcaram = 0;
     }
-  mutex_unlock(trava_passageiros_embarque);
+  pthread_mutex_unlock(trava_passageiros_embarque);
 
   sem_wait(fila_desembarque);
   desembarcar();
